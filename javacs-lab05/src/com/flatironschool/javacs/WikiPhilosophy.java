0a3a4a7df77063b28b8d056da8f434d524eebf59
Agustin Lau <alau7@gatech.edu>
2016-06-05 19:07:19 -0500

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -31,21 +31,108 @@ public class WikiPhilosophy {
 		
         // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		// String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		// Elements paragraphs = wf.fetchWikipedia(url);
 
-		Element firstPara = paragraphs.get(0);
+		// Element firstPara = paragraphs.get(0);
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		// Iterable<Node> iter = new WikiNodeIterable(firstPara);
+		// for (Node node: iter) {
+		// 	if (node instanceof TextNode) {
+		// 		System.out.print(node);
+		// 	}
+  //       }
 
         // the following throws an exception so the test fails
         // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+        // String msg = "Complete this lab by adding your code and removing this statement.";
+        // throw new UnsupportedOperationException(msg);
+
+		List<String> urlList = new ArrayList<String>();
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+		boolean philosophy = false;
+		while (!philosophy) {
+			Elements paragraphs = wf.fetchWikipedia(url);
+			urlList.add(url);
+			Element firstPara = paragraphs.get(0);
+			Iterable<Node> iter = new WikiNodeIterable(firstPara);
+			String firstValidURL = getFirstValidURL(iter, url);
+			if (firstValidURL != null) {
+				if (url.equals("https://en.wikipedia.org/wiki/Philosophy")) {
+					urlList.add(firstValidURL);
+					philosophy = true;
+				
+				} else {
+					url = firstValidURL;
+				}
+			} else {
+				philosophy = true;
+				System.out.println("An error has occurred!");
+			}
+			
+			
+		}
+
+		for (String visited : urlList) {
+			System.out.println(visited);
+		}
 	}
+
+	public static String getFirstValidURL(Iterable<Node> iter, String currentUrl)  {
+		for (Node node : iter) {
+			if (node instanceof Element) {
+				Element element = (Element) node;
+				if (isValid(element, currentUrl)) {
+					return "https://en.wikipedia.org" + node.attr("href");
+				}
+			}
+		}
+		return null;
+	}
+
+	public static boolean isValid(Element element, String currentURL) {
+		String link = "https://en.wikipedia.org" + element.attr("href");
+		if (link.equals(currentURL)) {
+			return false;
+		}
+		if (isItalics(element) || isEmpty(element) || isHelpURL(element) || isCitation(element)) {
+			return false;
+		}
+		return true;
+
+	}
+
+	public static boolean isItalics(Element element) {
+		Element current = element;
+		while (current != null) {
+			if (current.tagName().equals("i") || current.tagName().equals("em")) {
+				return true;
+			}
+			current = current.parent();
+		}
+		return false;
+	}
+
+	public static boolean isEmpty(Element element) {
+		if (element.attr("href").equals("")) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isHelpURL(Element element) {
+		if (element.attr("href").startsWith("/wiki/Help:")) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isCitation(Element element) {
+		if (element.attr("href").startsWith("#cite_note")) {
+			return true;
+		}
+		return false;
+	}
+
 }
